c07a9835472e185b9edf012f60ba72ff8b93b20d
Shailja Gupta <115636055+shailja026@users.noreply.github.com>
2023-12-17 12:01:18 +0530

    all done now

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
+const cors = require("cors");
+server.use(cors());
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
 // Add this before server.use(router)
 server.use(
  // Add custom route here if needed
