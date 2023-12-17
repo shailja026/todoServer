b276ad8b8e876d58286d5b46312640415aa9ab90
Shailja Gupta <115636055+shailja026@users.noreply.github.com>
2023-12-17 11:27:08 +0530

    configuration done

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const dotenv = require("dotenv");
-const cors = require("cors");
-server.use(cors)
-dotenv.config()
-const port = process.env.PORT | 3001
-server.use(middlewares)
-server.use(router)
-server.listen(port, () => {
-  console.log(`JSON Server is running ${port}`)
-})
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
