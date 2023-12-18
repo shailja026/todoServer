5d04df2888637d6be0a98f72d573e91d6c78c3bd
Shailja Gupta <115636055+shailja026@users.noreply.github.com>
2023-12-18 12:31:05 +0530

    endpoints

--- index.js
+++ index.js
@@ -6,30 +6,28 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT | 7000;
-server.use(cors({
-    origin: '*', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+server.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204,
-   
-  }));
+  })
+);
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-// Add this before server.use(router)
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
+  jsonServer.rewriter({
+    "/*": "/$1",
+  })
 );
 server.use(router);
-// Listen to port
+
 server.listen(port, () => {
- console.log(`JSON Server is running on port ${port}`);
+  console.log(`JSON Server is running on port ${port}`);
 });
 
-// Export the Server API
 module.exports = server;
